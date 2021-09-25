e74870737c633a0fa744cadcad6886459c976669
Justin Vargas <90952647+JustinVargas0915@users.noreply.github.com>
2021-09-25 15:31:04 +0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="https://www.pngitem.com/pimgs/m/109-1092592_pogchamp-emote-png-transparent-background-poggers-emote-png.png" className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload ------POGGERS!!.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
