08fd41e35d4029939750cede5b565ec8989cb724
Justin Vargas <90952647+JustinVargas0915@users.noreply.github.com>
2021-11-10 13:45:16 +0800

    commit

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -15,11 +15,32 @@ class Button extends React.Component{
         )
     }
 
+    decreaseCounter(){
+        this.setState(
+            {
+                count: this.state.count-1
+            }
+        )
+    }
+
+
+    reset(){
+        this.setState(
+            {
+                count: 0
+            }
+        )
+    }
+
     render(){
         return(
             <div>
-                <button onClick ={this.addCounter}>click me</button>
-                <p>counts = {this.state.count}</p>
+                <button className='inc' onClick={(e) => this.addCounter(e)}>Add Counter</button>
+                <button className='dec' onClick={(e) => this.decreaseCounter(e)}>Decrease Counter</button>
+                <button className='reset' onClick={(e) => this.reset(e)}>Reset Counter</button>
+                <h1>Current Count: {this.state.count}</h1>
+
+                
             </div>
         )
            
